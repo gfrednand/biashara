e7e0d326da10f86f9218b47c68863ecd505f7525
Chami <gfrednand@gmail.com>
2018-07-03 18:19:18 +0300

    test blade

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
 <div class="jumbotron ">
-      {{$name}}
+    @foreach($name as $n)
+<h3><a href="#">{{$n->name}}</a></h3>
+    <small>Written On: {{$n->created_at}}</small>
+    @endforeach
+     
 </div>
 
 @endsection
